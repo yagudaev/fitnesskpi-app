561c8b7c2f4515f54d75facb51f616d3200892fb
Michael Yagudaev <michael@yagudaev.com>
2015-09-20 15:27:04 -0400

    Revert "another try to fix meteor cors"
    This reverts commit 7be0e9604e3ed5c3047093e6e7d3c11d5be2fbb1.

--- server/fitnesskpi-app.js
+++ server/fitnesskpi-app.js
@@ -1,6 +1,7 @@
 Meteor.startup(function () {
-  WebApp.connectHandlers.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return next();
-  });
+});
+
+WebApp.connectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
 });
