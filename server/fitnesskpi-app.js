7be0e9604e3ed5c3047093e6e7d3c11d5be2fbb1
Michael Yagudaev <michael@yagudaev.com>
2015-09-20 15:02:59 -0400

    another try to fix meteor cors

--- server/fitnesskpi-app.js
+++ server/fitnesskpi-app.js
@@ -1,7 +1,6 @@
 Meteor.startup(function () {
-});
-
-WebApp.connectHandlers.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return next();
+  WebApp.connectHandlers.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return next();
+  });
 });
